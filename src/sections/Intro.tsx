0923a4059d868f5954ccb3c2ca31a3280aa7dffe
christiechindy <chindychristie@gmail.com>
2024-05-02 15:29:31 +0800

    change me desc

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -2,7 +2,7 @@ const Intro = () => {
     return (
         <section className="intro">
             <div className="name">Hello, I'm <span>Chindy</span></div>
-            <div className="info">A 7th semester student pursuing a degree in Informatics Engineering. Interested in <span>front-end</span> and <span>back-end</span> web development, as well as AI / <span>Deep Learning</span>.</div>
+            <div className="info">Eager learner with a strong foundation in <span>front-end development</span> and a growing <span>back-end</span> skillset.</div>
         </section>
     )
 }
